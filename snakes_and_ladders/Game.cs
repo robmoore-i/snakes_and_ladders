82e0332b44eed68df24f9d380c348961a6fa7950
romo <robert.moore@zuhlke.com>
2019-11-08 14:32:36 +0000

    Reorder methods

--- snakes_and_ladders/Game.cs
+++ snakes_and_ladders/Game.cs
@@ -14,13 +14,13 @@ namespace snakes_and_ladders {
             numberOfPlayers = this.players.Count;
         }
 
+        public List<string> PlayerNames() {
+            return players.Select(player => player.name).ToList();
+        }
+
         public void Start() {
             string firstPlayerName = players[0].name;
             console.Print(firstPlayerName + ", press enter to roll the dice.");
-        }
-
-        public List<string> PlayerNames() {
-            return players.Select(player => player.name).ToList();
         }
     }
 }
